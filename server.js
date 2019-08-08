25f5639bb5c0c7cd161a70c5b0a5a51ebdb90bce
Dylan Li <dylanlix@gmail.com>
2019-08-08 15:25:10 -0400

    updated register function

--- server.js
+++ server.js
@@ -21,4 +21,50 @@ app.get('/login', function(req, res) {
   res.send("Test");
 });
 
+app.get('/register', function(req, res) {
+  var username = req["username"];
+  var email = req["email"];
+
+  var user_ret = readData({"username": username});
+  var email_ret = readData({"email": email});
+  if (!Object.keys(user_ret).length || !Object.keys(email_ret).length) {
+  	res.send("Username or email is taken");
+  }
+});
+
+// registers users, asynchronous commands
+function registerUser(req, username, email) {
+  	var password = req.query.password;
+	var event_type = req.query.event_type;
+	var event_size = req.query.event_size;
+	var money_req = req.query.money_req;
+
+	data = JSON.stringify({"username": username, "email": email, "password": password, 
+		  						"event_type": event_type, "money_req": money_req, , "size": event_size});
+
+	success = writeData("users", username, data);
+
+	if (success==0) {
+		res.send("Database query error");
+	}
+	else{
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "X-Requested-With");
+		res.send("Success!");
+	}
+}
+
+
+app.get('/sprints', function(req, res)) {
+	user = req.query.username;
+
+}
+
+
+app.get('/event', function(req, res)) {
+	user = req["username"];
+
+}
+
+
 app.listen(port, function() {}); //starts the server, alternatively you can use app.listen(port)
